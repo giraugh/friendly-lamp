c5116b74bcb3ed98ab3510f1215b6f65986a8d8c
retroverse <ebreakey@gmail.com>
2018-02-21 17:35:46 +1100

    Add CORS header

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/auth', authControl)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const APP_PORT = process.env.PORT || '3002'
 
