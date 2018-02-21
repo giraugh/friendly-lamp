aeb721ac3096a56460ba33d497a36fcbec55bcf0
retroverse <ebreakey@gmail.com>
2018-02-21 22:40:39 +1100

    Update allowed CORS headers

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const {
 const app = express()
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token, X-Access-Token')
   next()
 })
 app.use(bodyParser.urlencoded({ extended: false }))
