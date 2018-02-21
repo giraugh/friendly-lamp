d6365c9803e3d44287a79c3e336bc864cb200c20
retroverse <ebreakey@gmail.com>
2018-02-21 18:27:19 +1100

    Reorder middleware

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const {
 } = require('./src/data')
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use('/auth', authControl)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use('/auth', authControl)
 
 const APP_PORT = process.env.PORT || '3002'
 
